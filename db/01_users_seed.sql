cf670357995b5e64e6f684dcc7b6e7bddbe5c5a9
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-09-14 11:01:55 -0700

    Create backend repository

---
+++ db/01_users_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (spotify_name, spotify_user_id) VALUES ( '12345', '12');
+INSERT INTO users (spotify_name, spotify_user_id) VALUES ('abcde', '13');
+INSERT INTO users (spotify_name, spotify_user_id) VALUES ('pqrst', '15');
+
+Footer
